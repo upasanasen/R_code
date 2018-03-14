5b176183e0a707e6965022ab45c755ac61b5d8f7
Upasana Sen <upasana19sen95@gmail.com>
2018-03-14 23:38:26 +0530

    my 2nd R code
    handling of missing values

--- R_work_missing_value_2.R
+++ R_work_missing_value_2.R
@@ -1,3 +1,4 @@
-d= c( 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-d[10:19]= NA
-d
+#handling missig values
+d= c( 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20) #create a vector
+d[10:19]= NA #represent the missing value
+d #then run
